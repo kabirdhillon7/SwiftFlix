848101ed6f4fbad982ac7d8d1fef802c73bd469a
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-05-18 14:53:00 -0700

    Initial Commit

---
+++ MovieFlix/MovieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by Kabir Dhillon on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
