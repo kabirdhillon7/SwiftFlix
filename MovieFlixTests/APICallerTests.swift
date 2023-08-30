124ceddf5eb2667f86283c00b85d1378fc27a8de
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-08-29 22:29:22 -0700

    APICallerTests

--- MovieFlixTests/APICallerTests.swift
+++ MovieFlixTests/APICallerTests.swift
@@ -29,27 +29,24 @@ class MockAPIService: DataServicing {
 
 final class APICallerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var apiCaller: APICaller!
+    
+    override func setUp() {
+        super.setUp()
+        apiCaller = APICaller()
+    }
+    
+    override func tearDown() {
+        apiCaller = nil
+        super.tearDown()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_getMovies_shouldNotBeNil() {
+        // given
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=") else {
+            return
         }
+        
+        XCTAssertNotNil(apiCaller.getMovies(toUrl: url))
     }
-
 }
